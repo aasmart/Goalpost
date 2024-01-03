d2024b2efd0d4a6182083fba9f3156e8647089f7
aasmart <48771274+aasmart@users.noreply.github.com>
2024-01-03 14:42:37 -0500

    Remove unused imports

--- app/src/main/java/io/aasmart/goalpost/compose/screens/GoalsReflectionScreen.kt
+++ app/src/main/java/io/aasmart/goalpost/compose/screens/GoalsReflectionScreen.kt
@@ -15,8 +15,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -27,7 +25,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
