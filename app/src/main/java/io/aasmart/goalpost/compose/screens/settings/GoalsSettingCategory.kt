ca330114d318494379bc1ed2172d151c1a78eac5
aasmart <48771274+aasmart@users.noreply.github.com>
2024-01-08 15:58:57 -0500

    Clean up goal settings category by moving part of it into a component

--- app/src/main/java/io/aasmart/goalpost/compose/screens/settings/GoalsSettingCategory.kt
+++ app/src/main/java/io/aasmart/goalpost/compose/screens/settings/GoalsSettingCategory.kt
@@ -1,7 +1,5 @@
 package io.aasmart.goalpost.compose.screens.settings
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,17 +12,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,19 +28,32 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.aasmart.goalpost.R
-import io.aasmart.goalpost.compose.components.TimePickerDialog
+import io.aasmart.goalpost.compose.components.TextFieldTimePicker
 import io.aasmart.goalpost.data.settingsDataStore
 import kotlinx.coroutines.launch
+
+/** Converts a time in milliseconds to hours and minutes
+ *
+ * @param millis The time in milliseconds
+ * @return A pair where the first value is the hours, and the second is minutes
+ */
+private fun millisToHoursAndMinutes(millis: Long): Pair<Int, Int> {
+    val reflectionHours = millis
+        .div(1000 * 60 * 60)
+        .toInt()
+    val reflectionMinutes = millis
+        .minus(reflectionHours.times(1000 * 60 * 60))
+        .div(1000 * 60)
+        .toInt()
+
+    return reflectionHours to reflectionMinutes
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GoalCategoryContent() {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
-
-    var timePickerDialogExpanded by rememberSaveable {
-        mutableStateOf(false)
-    }
 
     val settingsState by context.settingsDataStore.data.collectAsStateWithLifecycle(initialValue = null)
     if(settingsState == null) {
@@ -63,114 +70,70 @@ fun GoalCategoryContent() {
         return
     }
 
-    /*Convert reflection time in MS to hours and minutes in order
-    * to be displayed */
-    val reflectionHours = settingsState
-        ?.goalReflectionTimeMs
-        ?.div(1000 * 60 * 60)
-        ?.toInt()
-    val reflectionMinutes = settingsState
-        ?.goalReflectionTimeMs
-        ?.minus(reflectionHours?.times(1000 * 60 * 60) ?: 0)
-        ?.div(1000 * 60)
-        ?.toInt()
-
-    var selectedHour by remember {
-        mutableIntStateOf(reflectionHours ?: 0)
+    var reflectionTimePickerDialogVisible by remember {
+        mutableStateOf(false)
     }
-    var selectedMinute by remember {
-        mutableIntStateOf(reflectionMinutes ?: 0)
+    val selectedReflectionTime = settingsState?.goalReflectionTimeMs?.let {
+        millisToHoursAndMinutes(it)
+    } ?: (0 to 0)
+
+    var reflectionSelectedHours by remember {
+        mutableIntStateOf(selectedReflectionTime.first)
+    }
+    var reflectionSelectedMinutes by remember {
+        mutableIntStateOf(selectedReflectionTime.second)
     }
 
     // Create the label displayed in the text field
-    val selectedMinuteStr = if(selectedMinute < 10) "0${selectedMinute}" else selectedMinute
-    val selectedTimeStr =
-        if(selectedHour > 12)
-            "${selectedHour - 12}:${selectedMinuteStr} PM"
-        else if(selectedHour == 0)
-            "12:${selectedMinuteStr} AM"
-        else if(selectedHour == 12)
-            "12:${selectedMinuteStr} PM"
-        else
-            "${selectedHour}:${selectedMinuteStr} AM"
 
-    TextField(
-        value = selectedTimeStr,
-        onValueChange = {},
+    val reflectionTimePickerState = rememberTimePickerState(
+        reflectionSelectedHours,
+        reflectionSelectedMinutes,
+        false
+    )
+
+    TextFieldTimePicker(
+        timePickerState = reflectionTimePickerState,
+        selectedHours = reflectionSelectedHours,
+        selectedMinutes = reflectionSelectedMinutes,
+        onTimePickerSubmit = { hour, minute ->
+            reflectionSelectedHours = hour
+            reflectionSelectedMinutes = minute
+
+            val selectedMillis =
+                (reflectionSelectedHours * 3600 + reflectionSelectedMinutes * 60) * 1000L
+
+            coroutineScope.launch {
+                context.settingsDataStore.updateData {
+                    return@updateData it.toBuilder()
+                        .setGoalReflectionTimeMs(selectedMillis)
+                        .build()
+                }
+            }
+
+            reflectionTimePickerDialogVisible = false
+        },
+        timePickerDialogVisible = reflectionTimePickerDialogVisible,
+        onVisibilityChanged = { reflectionTimePickerDialogVisible = it },
         label = {
             Text(text = stringResource(id = R.string.reflection_time))
         },
-        readOnly = true,
         leadingIcon = {
             Icon(Icons.Filled.DateRange, contentDescription = null)
         },
         supportingText = {
-             Text(
-                 text = stringResource(id = R.string.reflection_time_description),
-                 fontWeight = FontWeight.Light
-             )
+            Text(
+                text = stringResource(id = R.string.reflection_time_description),
+                fontWeight = FontWeight.Light
+            )
         },
-        interactionSource = remember { MutableInteractionSource() }
-            .also { interactionSource ->
-                LaunchedEffect(interactionSource) {
-                    interactionSource.interactions.collect {
-                        if (it !is PressInteraction.Release)
-                            return@collect
-                        timePickerDialogExpanded = true
-                    }
-                }
-            },
+        timePickerDialogLabel = {
+            Text(
+                text = stringResource(id = R.string.reflection_time_picker),
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(8.dp)
+            )
+        },
         modifier = Modifier.fillMaxWidth()
     )
-
-    // Create the time picker
-    val timePickerState = rememberTimePickerState(
-        selectedHour,
-        selectedMinute,
-        false
-    )
-
-    val submitReflectionTimePicker = {
-        selectedHour = timePickerState.hour
-        selectedMinute = timePickerState.minute
-
-        val selectedMillis =
-            (selectedHour * 3600 + selectedMinute * 60) * 1000L
-
-        coroutineScope.launch {
-            context.settingsDataStore.updateData {
-                return@updateData it.toBuilder()
-                    .setGoalReflectionTimeMs(selectedMillis)
-                    .build()
-            }
-        }
-
-        timePickerDialogExpanded = false
-    }
-
-    if(timePickerDialogExpanded) {
-        TimePickerDialog(
-            label = {
-                Text(
-                    text = stringResource(id = R.string.reflection_time_picker),
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.padding(8.dp)
-                )
-            },
-            timePickerState = timePickerState,
-            onExpandedChange = { timePickerDialogExpanded = it },
-            dismiss = {
-                TextButton(onClick = { timePickerDialogExpanded = false }) {
-                    Text(text = stringResource(id = R.string.dismiss))
-                }
-            },
-            confirm = {
-                TextButton(
-                    onClick = submitReflectionTimePicker
-                ) {
-                    Text(text = stringResource(id = R.string.confirm))
-                }
-            }
-        )
-    }
 }
