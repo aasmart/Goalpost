31ee259490c52684987fcdba780761e0138a1e6e
aasmart <alexandersmart2005@gmail.com>
2024-02-26 16:52:36 -0500

    Add coming soon text to goal calendar

--- app/src/main/java/io/aasmart/goalpost/compose/screens/GoalCalendarScreen.kt
+++ app/src/main/java/io/aasmart/goalpost/compose/screens/GoalCalendarScreen.kt
@@ -3,7 +3,9 @@ package io.aasmart.goalpost.compose.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import io.aasmart.goalpost.compose.GoalpostNav
 import io.aasmart.goalpost.compose.GoalpostNavScaffold
@@ -14,11 +16,12 @@ fun GoalCalendarScreen(
 ) {
     GoalpostNavScaffold(nav = goalpostNav) {
         Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .padding(it)
                 .fillMaxSize()
         ) {
-
+            Text(text = "Coming soon...")
         }
     }
 }
